39f8c6550d32c74f982db778ee80a018a2d5ffc3
YeoHoonYun <cjswo9207@gmail.com>
2020-05-18 10:39:44 +0900

    commit

--- app.py
+++ app.py
@@ -4,11 +4,28 @@ import json
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import DateTime
+from flask_swagger import swagger
 from pytz import timezone
 
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:root@localhost/test_db"
-db = SQLAlchemy(app, session_options={'autocommit': True})
+from flask_restplus import Api, Resource
+
+flask_app  = Flask(__name__)
+app = Api(app = flask_app)
+
+flask_app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:root@localhost/test_db"
+db = SQLAlchemy(flask_app, session_options={'autocommit': True})
+name_space = app.namespace('main', description='Main APIs')
+
+@name_space.route("/")
+class MainClass(Resource):
+	def get(self):
+		return {
+			"status": "Got new data"
+		}
+	def post(self):
+		return {
+			"status": "Posted new data"
+		}
 
 # 클래스명이 테이블명
 class User_test(db.Model):
@@ -30,10 +47,16 @@ users = User_test.query.all()
 # engine = create_engine("mysql+pymysql://root:hadoop@localhost/test_db", encoding='utf-8')
 # conn = engine.connect()
 
-@app.route('/')
+@flask_app.route('/')
 def hello_world():
     return ""
 
+@flask_app.route("/spec")
+def spec():
+    swag = swagger(flask_app)
+    swag['info']['version'] = "2.0"
+    swag['info']['title'] = "My API"
+    return jsonify(swag)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    flask_app.run(debug=True)
